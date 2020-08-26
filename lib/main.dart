c30e0216a5a1fc97b8af3f10490e5282ee1554d8
Yudi Setiawan <kolonel.yudisetiawan@gmail.com>
2020-08-27 00:01:09 +0700

    Buat UI dan case error

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,213 @@
-import 'package:flutter/material.dart';
+import 'dart:io';
 
-void main() {
-  runApp(MyApp());
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() => runApp(App());
+
+class App extends StatefulWidget {
+  @override
+  _AppState createState() => _AppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _AppState extends State<App> {
+  var isLogin = false;
+
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) async {
+      var sharedPreferences = await SharedPreferences.getInstance();
+      isLogin = sharedPreferences.getBool('isLogin') ?? false;
+      if (isLogin) {
+        setState(() {});
+      }
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: !isLogin ? LoginPage() : HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
+Future<dynamic> onBackgroundMessageHandler(Map<String, dynamic> message) async {
+  debugPrint('onBackgroundMessageHandler');
+  var sharedPreferences = await SharedPreferences.getInstance();
+  var isLogin = sharedPreferences.getBool('isLogin') ?? false;
+  if (isLogin) {
+    // TODO: tampilkan lokal notifikasi
+    var content = '-';
+    if (Platform.isIOS) {
+      content = message['content'];
+    } else {
+      content = message['data']['content'];
+    }
+    debugPrint('content: $content');
+  }
+  return true;
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class _LoginPageState extends State<LoginPage> {
+  final scaffoldState = GlobalKey<ScaffoldState>();
+  final formState = GlobalKey<FormState>();
+  final controllerEmail = TextEditingController();
+  final controllerPassword = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      key: scaffoldState,
+      body: SafeArea(
+        child: Container(
+          width: double.infinity,
+          padding: EdgeInsets.symmetric(horizontal: 24),
+          child: Form(
+            key: formState,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Welcome Back',
+                  style: Theme.of(context).textTheme.headline6,
+                ),
+                Text(
+                  'Sign to continue',
+                  style: Theme.of(context).textTheme.caption,
+                ),
+                SizedBox(height: 42),
+                TextFormField(
+                  controller: controllerEmail,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.email),
+                    border: OutlineInputBorder(),
+                    isDense: true,
+                    labelText: 'EMAIL',
+                  ),
+                  keyboardType: TextInputType.emailAddress,
+                  validator: (value) {
+                    return value.isEmpty ? 'Enter an email' : null;
+                  },
+                ),
+                SizedBox(height: 16),
+                TextFormField(
+                  controller: controllerPassword,
+                  decoration: InputDecoration(
+                    prefixIcon: Icon(Icons.lock),
+                    border: OutlineInputBorder(),
+                    isDense: true,
+                    labelText: 'PASSWORD',
+                  ),
+                  validator: (value) {
+                    return value.isEmpty ? 'Enter a password' : null;
+                  },
+                  obscureText: true,
+                  keyboardType: TextInputType.text,
+                ),
+                SizedBox(height: 16),
+                SizedBox(
+                  width: double.infinity,
+                  height: 42,
+                  child: RaisedButton(
+                    onPressed: () async {
+                      if (formState.currentState.validate()) {
+                        var email = controllerEmail.text.trim();
+                        var password = controllerPassword.text.trim();
+                        if (email == 'admin' && password == 'admin') {
+                          var sharedPreferences = await SharedPreferences.getInstance();
+                          await sharedPreferences.setBool('isLogin', true);
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => HomePage()));
+                        } else {
+                          scaffoldState.currentState.showSnackBar(SnackBar(content: Text('Login failed')));
+                        }
+                      }
+                    },
+                    child: Text('LOGIN'),
+                    color: Colors.blue,
+                    textColor: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final firebaseMessaging = FirebaseMessaging();
+  var tokenFcm = '-';
+
+  @override
+  void initState() {
+    firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        _getDataFcm(message);
+      },
+      onBackgroundMessage: onBackgroundMessageHandler,
+      onResume: (Map<String, dynamic> message) async {
+        _getDataFcm(message);
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        _getDataFcm(message);
+      },
+    );
+    super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.symmetric(horizontal: 16),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            RaisedButton(
+              child: Text('GET TOKEN'),
+              onPressed: () {
+                firebaseMessaging.requestNotificationPermissions(
+                  const IosNotificationSettings(),
+                );
+                firebaseMessaging.onIosSettingsRegistered.listen((event) {
+                  debugPrint('IOS settings registered');
+                });
+                firebaseMessaging.getToken().then((value) => setState(() {
+                  tokenFcm = value;
+                  debugPrint('tokenFcm: $tokenFcm');
+                }));
+              },
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Text(tokenFcm),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  void _getDataFcm(Map<String, dynamic> message) {
+    var content = '';
+    if (Platform.isIOS) {
+      content = message['content'];
+    } else {
+      content = message['data']['content'];
+    }
+    debugPrint('content: $content');
   }
 }
